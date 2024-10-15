a18abd04e382e97b694eba3cae79a978dac41b83
kite-house <hodosok1301@gmail.com>
2024-10-16 06:37:48 +0700

    not binary

--- parser.py
+++ parser.py
@@ -12,7 +12,6 @@ options = Options()
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
-options.binary_location = "webdriver/chrome/chrome"
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 async def parserJournal(username: str, password: str) -> dict:
