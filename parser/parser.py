8022e53308c76bda2c73ee1183dbd25293f1da42
kite-house <hodosok1301@gmail.com>
2024-10-25 06:40:39 +0700

    fix

--- parser/parser.py
+++ parser/parser.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from settings import *
+from parser.settings import *
 import asyncio
 
 service = Service(ChromeDriverManager().install())
