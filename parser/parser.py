10fcd9dccf8ac639884f5b31b8b47abfd3a468f3
kite-house <hodosok1301@gmail.com>
2024-10-25 07:34:46 +0700

    fix

--- parser/parser.py
+++ parser/parser.py
@@ -16,22 +16,14 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 async def parserJournal(username: str, password: str) -> dict:
-    print('starting browser...')
     driver = webdriver.Chrome(options=options, service=service)
     driver.get(URL)
-    print('start browser!')
 
     await asyncio.sleep(10)
 
-    print('auth start...')
-
     driver.find_element(By.ID, 'username').send_keys(username)
-    print('succes send username!')
     driver.find_element(By.ID, 'password').send_keys(password)
-    print('succes send password!')
     driver.find_element(By.XPATH, BUTTON_AUTH).click()
-
-    print('auth succes!')
 
     await asyncio.sleep(10)
 
@@ -43,8 +35,6 @@ async def parserJournal(username: str, password: str) -> dict:
                 driver.find_element(By.XPATH, NOTIFICATION).click() # Убераем уведомление 
             except Exception:
                 await asyncio.sleep(1)
-
-            print('start parsing...')
 
             data = {'homework': {}}
             data['name'] = driver.find_element(By.XPATH, NAME).text
